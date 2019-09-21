456930230997a0ea18f3350fc0e8be23754bc8f3
janeDoe7 <50467708+janeDoe7@users.noreply.github.com>
2019-09-21 00:00:01 -0400

    partially working rotation

--- src/Submarine.java
+++ src/Submarine.java
@@ -1,17 +1,24 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-public class Turret extends Application {
+import java.util.EventListener;
+
+public class Submarine extends Application {
     private static Stage stage = new Stage();
     private static Group root = new Group();
     private static Canvas canvas = new Canvas(1600, 900);
+    private static Pane pane = new Pane();
 
     public static void main(String[] args) {
         launch(args);
@@ -19,13 +26,14 @@ public class Turret extends Application {
 
     public void start(Stage initStage) {
         initStage = stage;
-        initStage.setTitle("Turret");
+        initStage.setTitle("Submarine!");
         Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
         double w = screenSize.getWidth();
         double h = screenSize.getHeight();
         Canvas canvas = new Canvas(w, h);
         GraphicsContext ctx = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
+        root.getChildren().add(pane);
         Scene scene = new Scene(root);
         initStage.setX(screenSize.getMinX());
         initStage.setY(screenSize.getMinY());
@@ -34,10 +42,16 @@ public class Turret extends Application {
         initStage.setHeight(h);
         initStage.show();
 
-        Image turretImg = new Image("File:./images/submarine.png");
-        ctx.drawImage(turretImg, (w / 2) - (turretImg.getWidth() / 2), (h / 2) - (turretImg.getHeight() / 2));
+        ImageView submarineView = new ImageView();
+        Image submarineImg = new Image("File:./images/submarine.png");
+        submarineView.setImage(submarineImg);
+        submarineView.setX(w / 2 - submarineImg.getWidth() / 2);
+        submarineView.setY(h / 2 - submarineImg.getHeight() / 2);
+        pane.getChildren().add(submarineView);
 
-
+        scene.setOnKeyPressed(
+                event -> submarineView.setRotate(submarineView.getRotate() + 90)
+        );
     }
 
 }
