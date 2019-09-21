d4b08f6423fb367b191b6d7722640ec5220c0132
janeDoe7 <50467708+janeDoe7@users.noreply.github.com>
2019-09-20 22:39:28 -0400

    add canvas

--- src/Turret.java
+++ src/Turret.java
@@ -1,26 +1,38 @@
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Turret extends Application {
-    private static BorderPane borderPane = new BorderPane();
-    private static GridPane gridPane = new GridPane();
-    private Scene scene = new Scene(borderPane);
     private static Stage stage = new Stage();
-
-    public void start(Stage initStage) {
-        stage = initStage;
-        stage.setScene(scene);
-        stage.show();
-    }
+    private static Group root = new Group();
+    private static Canvas canvas = new Canvas(1600, 900);
+    private static GraphicsContext context = canvas.getGraphicsContext2D();
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public static void launch(String[] args) {
+    public void start(Stage initStage) {
+        initStage = stage;
+        initStage.setTitle("Turret");
+        Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
+        double w = screenSize.getWidth();
+        double h = screenSize.getHeight();
+        Canvas canvas = new Canvas(w, h);
+        GraphicsContext ctx = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        initStage.setX(screenSize.getMinX());
+        initStage.setY(screenSize.getMinY());
+        initStage.setScene(scene);
+        initStage.setWidth(w);
+        initStage.setHeight(h);
+        initStage.show();
     }
 
 }
